fa489a35ab8c9fbf23078953bfc50f635c8a0fe5
bat oro <batoro@mydebian>
2014-07-31 16:26:36 +0300

    Update copyrights

--- cport/detail/task_handler_base.hpp
+++ cport/detail/task_handler_base.hpp
@@ -4,7 +4,7 @@
 //
 // task_handler_base.hpp
 //
-// Copyright (c) 2013 Orlin Hristov (orlin dot hristov at gmail dot com)
+// Copyright (c) 2013-2014 Orlin Hristov (orlin dot hristov at gmail dot com)
 //
 // Distributed under the Apache License, Version 2.0
 // visit http://www.apache.org/licenses/ for more information.
